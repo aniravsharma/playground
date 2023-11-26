9919bbb29e099cee05cdd0899ce6749bdc987182
aniravsharma <aniravsharma5@gmail.com>
2023-11-26 11:34:25 +0800

    my first code

---
+++ calculator.py
@@ -0,0 +1,23 @@
+
+""""
+
+Computer does not know any thing about value.
+
+First things you have to tell what is the type 
+
+for eg 
+10 - it is number 
+Ansh - it is String 
+21/05/2011 - date 
+20.99 - Amount 
+
+"""
+
+
+x = input("What is your first number >: ")
+y = input("What is your second number >: ")
+
+z = float(x) * float(y)
+
+print( z)
+
---
+++ helloworld.py
@@ -0,0 +1,1 @@
+print("Hello World. Today is my first day")
